beb0c4c51dc5c1950fbf16d56f472f5bb1f7f56a
alexx99gg <alex.go.go.99@gmail.com>
2019-10-19 18:58:33 +0200

    web now public!

---
+++ gulpfile.js
@@ -0,0 +1,107 @@
+var gulp = require('gulp');
+
+// gulp plugins and utils
+var gutil = require('gulp-util');
+var livereload = require('gulp-livereload');
+var nodemon = require('gulp-nodemon');
+var postcss = require('gulp-postcss');
+var sourcemaps = require('gulp-sourcemaps');
+var zip = require('gulp-zip');
+
+// postcss plugins
+var autoprefixer = require('autoprefixer');
+var colorFunction = require('postcss-color-function');
+var cssnano = require('cssnano');
+var customProperties = require('postcss-custom-properties');
+var easyimport = require('postcss-easy-import');
+
+// img plugins
+var imageop = require('gulp-image-optimization');
+
+var swallowError = function swallowError(error) {
+    gutil.log(error.toString());
+    gutil.beep();
+    this.emit('end');
+};
+
+var nodemonServerInit = function () {
+    livereload.listen(1234);
+};
+
+gulp.task('build', ['css', 'images'], function (/* cb */) {
+    return nodemonServerInit();
+});
+
+gulp.task('css', function () {
+    var processors = [
+        easyimport,
+        customProperties,
+        colorFunction(),
+        autoprefixer({browsers: ['last 2 versions']}),
+        cssnano()
+    ];
+
+    return gulp.src('src/css/**/*.css')
+        .on('error', swallowError)
+        .pipe(sourcemaps.init())
+        .pipe(postcss(processors))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('assets/built/'))
+        .pipe(livereload());
+});
+
+gulp.task('watch', function () {
+    gulp.watch('src/css/**', ['css']);
+});
+
+gulp.task('zip', ['css'], function() {
+    var targetDir = 'dist/';
+    var themeName = require('./package.json').name;
+    var filename = themeName + '.zip';
+
+    return gulp.src([
+        '**',
+        '!node_modules', '!node_modules/**',
+        '!dist', '!dist/**'
+    ])
+        .pipe(zip(filename))
+        .pipe(gulp.dest(targetDir));
+});
+
+gulp.task('default', ['build'], function () {
+    gulp.start('watch');
+});
+
+/*
+gulp.task('images', function(cb) {
+    gulp.src(['src/** /*.png','src/** /*.jpg','src/** /*.gif','src/** /*.jpeg']).pipe(imageop({
+        optimizationLevel: 5,
+        progressive: true,
+        interlaced: true
+    })).pipe(gulp.dest('assets/images')).on('end', cb).on('error', cb);
+});*/
+
+const imagemin = require('gulp-imagemin');
+
+
+gulp.task('images', () =>
+    {
+    gulp.src(['src/images/**/*.png', 'src/images/**/*.jpg', 'src/images/**/*.jpeg'])
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.jpegtran({progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ], {
+            verbose: true
+        }))
+        .pipe(gulp.dest('assets/images'));
+    gulp.src(['src/images/**/*.ico'])
+        .pipe(gulp.dest('assets/images'));
+    }
+);
